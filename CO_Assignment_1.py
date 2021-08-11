83f7bc68322f59eef4637478d9f90ad326c6eaec
mayankjha <mayank20521@iiitd.ac.in>
2021-08-11 11:37:51 +0530

    MJ2

--- CO_Assignment_1.py
+++ CO_Assignment_1.py
@@ -9,7 +9,6 @@ def bin(value, noOfDigits = 32 ):      # returns the binary value of the number
             x += "0"
         i = i // 2
     return x
-
 def registerValue(a):
     if a.lower() == 'r0':
         a = '000'
@@ -23,13 +22,12 @@ def registerValue(a):
         a = '100'
     elif a.lower() == 'r5':
         a = '101'
-    elif a.lower() =='r6':
+    elif a.lower() == 'r6':
         a = '110'
     elif a.lower() == 'flags':
         a = '111'
 
     return a
-
 functionOpcode = {'0': 'addition' ,
                   '1': 'subtraction',
                   '2': 'moveimmediate',
@@ -50,70 +48,74 @@ functionOpcode = {'0': 'addition' ,
                   '17': 'jumpifgreaterthan',
                   '18': 'jumpifequal',
                   '19': 'halt'}
+binOfReg = {
+    'r0': 0000000000000000,
+    'r1': 0000000000000000,
+    'r2': 0000000000000000,
+    'r3': 0000000000000000,
+    'r4': 0000000000000000,
+    'r5': 0000000000000000,
+}
+def binToDec(x):
+    num = 0
+    i = 0
+    while(x>0):
+        y = x%10
+        if(y==1):
+            num = num + pow(2,i)
+        i = i+1
+        x = (x - y)/10
+    return num
 
 
 
-def ADDITION():
-    pass
+
+def ADDITION(reg1, reg2, reg3):
+    # Case for addition operation:
+    # Only reg1 value is going to get changed.
+    # We send in three registers in order of their
+    # appearance in the instruction line.
+
+    return reg1
 
 def SUBTRACTION():
     pass
-
 def MOVEIMMEDIATE():
     pass
-
 def MOVEREGISTER():
     pass
-
 def LOAD():
     pass
-
 def STORE():
     pass
-
 def MULTIPLY():
     pass
-
 def DIVIDE():
     pass
-
 def RIGHTSHIFT():
     pass
-
 def LEFTSHIFT():
     pass
-
 def EXCLUSIVEOR():
     pass
-
 def OR():
     pass
-
 def AND():
     pass
-
 def INVERT():
     pass
-
 def COMPARE():
     pass
-
 def UNCONDITIONALJUMP():
     pass
-
 def JUMPIFLESSTHAN():
     pass
-
 def JUMPIFGREATERTHAN():
     pass
-
 def JUMPIFEQUAL():
     pass
-
 def HALT(s):
     return bin(int(return_key(s)), 5) + "00000000000"
-
-
 def return_key(val):
     for key, value in functionOpcode.items():
         if value == val:
@@ -123,6 +125,7 @@ def return_key(val):
 lst = [[]]
 x: str
 ans = []
+# ans is the 1d list in which all the binary values are being stored
 
 while True:
     line = input()
@@ -133,6 +136,20 @@ while True:
         continue
     else:
         lst = line.split(" ")
+
+        # MJ edits:
+        if lst[0] == 'add':
+            reg1 = lst[1]
+            reg2 = lst[2]
+            reg3 = lst[3]
+            reg1 = ADDITION(reg1, reg2,reg3)
+        # Case for addition operation:
+        # Only reg1 value is going to get changed.
+        # We send in three registers in order of their
+        # appearance in the instruction line.
+
+        #######################################
+
         if lst[0] == 'var':
             x = lst[1]
         if lst[0] == 'st' and lst[2] == x:
